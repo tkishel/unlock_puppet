42378cd630a14f523bc711da3ceecb9fcb1139b3
tkishel <tkishel@gmail.com>
2019-07-26 12:52:19 -0700

    remove full path to ruby and allow environment.bat to find ruby

--- files/unlock_puppet.bat
+++ files/unlock_puppet.bat
@@ -3,4 +3,4 @@ SETLOCAL
 
 call "C:\Program Files\Puppet Labs\Puppet\bin\environment.bat"
 
-"C:\Program Files\Puppet Labs\Puppet\sys\ruby\bin\ruby.exe" C:\ProgramData\PuppetLabs\puppet\unlock_puppet.rb
+ruby.exe C:\ProgramData\PuppetLabs\puppet\unlock_puppet.rb
